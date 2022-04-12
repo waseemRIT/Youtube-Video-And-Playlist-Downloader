a0a88150d8367cb03031da34d733371158c012d4
Muaz Osman <90202813+MuazOsmann@users.noreply.github.com>
2022-04-12 04:05:50 +0400

    Initial File

---
+++ Downloader.py
@@ -0,0 +1,6 @@
+from json.tool import main
+import PyQt5
+import sys
+from PyQt5.QtWidgets import  QApplication, QWidget, QPushButton, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QGridLayout, QFileDialog, QMessageBox, QMainWindow, QAction, QTextEdit
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import QFileDialog
