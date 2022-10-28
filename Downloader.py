02333e3ea5997a369124eb187c2d4c5e9a9678b8
Muaz Osman <90202813+MuazOsmann@users.noreply.github.com>
2022-10-29 00:12:45 +0400

    Reworked the Download Function
    wrote the download function with a different library.

--- Downloader.py
+++ Downloader.py
@@ -1,32 +1,17 @@
 from json.tool import main; import PyQt5
 import sys
 from PyQt5.QtWidgets import  QApplication, QPushButton, QLineEdit, QLabel, QFileDialog, QMessageBox, QMainWindow, QRadioButton, QFileDialog
-import youtube_dl
+import pytube
 
 #a function to download the video as a MP3 or MP4 file, takes the URL, the path and the format as parameters
 def VideoDownloader(url, path, type):
     try:
         if type == "MP3":
-            ydl_opts = {
-                'format': 'bestaudio/best',
-                'outtmpl': path + '/%(title)s.%(ext)s',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                    'preferredquality': '192',
-                }],
-            }
-        else:
-            ydl_opts = {
-                'format': 'bestvideo+bestaudio/best',
-                'outtmpl': path + '/%(title)s.%(ext)s',
-                'postprocessors': [{
-                    'key': 'FFmpegVideoConvertor',
-                    'preferedformat': 'mp4',
-                }],
-            }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
+            video = pytube.YouTube(url)
+            video.streams.filter(only_audio=True).first().download(path)
+        elif type == "MP4":
+            video = pytube.YouTube(url)
+            video.streams.filter(only_video=True).first().download(path)
     except:
         print("Error")
 
