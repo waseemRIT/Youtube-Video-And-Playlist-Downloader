3c4971884ab3b09a854d73e17492ea6226a7b8dc
Muaz Osman <90202813+MuazOsmann@users.noreply.github.com>
2022-10-28 23:51:42 +0400

    Created the Download function
    Created the download function to take the type of the file (MP4 or MP3) the path of where to download the file, and the URL of the video

--- Downloader.py
+++ Downloader.py
@@ -1,6 +1,31 @@
-from json.tool import main
-import PyQt5
+from json.tool import main; import PyQt5
 import sys
-from PyQt5.QtWidgets import  QApplication, QWidget, QPushButton, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QGridLayout, QFileDialog, QMessageBox, QMainWindow, QAction, QTextEdit
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import  QApplication, QPushButton, QLineEdit, QLabel, QFileDialog, QMessageBox, QMainWindow, QRadioButton, QFileDialog
+import youtube_dl
+
+#a function to download the video as a MP3 or MP4 file, takes the URL, the path and the format as parameters
+def VideoDownloader(url, path, type):
+    try:
+        if type == "MP3":
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'outtmpl': path + '/%(title)s.%(ext)s',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+            }
+        else:
+            ydl_opts = {
+                'format': 'bestvideo+bestaudio/best',
+                'outtmpl': path + '/%(title)s.%(ext)s',
+                'postprocessors': [{
+                    'key': 'FFmpegVideoConvertor',
+                    'preferedformat': 'mp4',
+                }],
+            }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+    except:
+        print("Error")
